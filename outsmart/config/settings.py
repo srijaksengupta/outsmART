f6583a711e24f496bcf41a20834d1cdf19b79c22
Srijak Sengupta <45115715+srijaksengupta@users.noreply.github.com>
2020-03-28 00:01:00 +0530

    Third inital setup commit

--- outsmart/config/settings.py
+++ outsmart/config/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '+r(r4u(4elpoppm!%m%&s*h12#4)^da*9@@#mlz$(=*gkq^6wx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'outsmart',
 ]
 
 MIDDLEWARE = [
