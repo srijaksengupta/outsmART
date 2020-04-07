99a2ad9c7756602f89cf50612e639cfa382cb0f7
Nad <nadimrahman98@gmail.com>
2020-04-07 17:18:02 +0100

    Transferring all files

--- outsmart/config/settings.py
+++ outsmart/config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- outsmart/config/urls.py
+++ outsmart/config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
 
 urlpatterns = [
+    path('',include('pages.urls')),
+    path('account/',include('account.urls')),
     path('admin/', admin.site.urls),
 ]
