0ce96f35aa14edaaa9bbecb4e68f77866e6f445c
Srijak Sengupta <45115715+srijaksengupta@users.noreply.github.com>
2020-04-17 03:01:16 +0530

    Fixed signup, login and logout issues

--- outsmart/account/views.py
+++ outsmart/account/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
+from django.contrib import messages
 from . import forms
 
 @login_required
 def info(request):
     return HttpResponse('Hello ' + request.user.username)
+
 
 def signup(request):
     context = {}
@@ -18,10 +20,11 @@ def signup(request):
         if form.is_valid():
             try:
               user = User.objects.create_user(
-                form.cleaned_data['username'], 
-                email=form.cleaned_data['email'], 
+                form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
                 password=form.cleaned_data['password'],
                 )
+              messages.success(request, 'Registration successful')
               return HttpResponseRedirect(reverse('account:login'))
             except IntegrityError:
                 form.add_error('username', 'Username is taken')
@@ -29,25 +32,30 @@ def signup(request):
         context['form'] = form   
     return render(request, 'account/signup.html', context)
 
+
 def do_login(request):
     context = {}
     if request.method == 'POST':
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            user = authenticate(request, 
-              username=form.cleaned_data['username'], 
-              password=form.cleaned_data['password'])
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                if 'next' in request.GET:
-                    return HttpResponseRedirect(request.GET['next'])
-                return HttpResponseRedirect(reverse('pages:index'))
+                next = request.GET.get('next')
+                if next:
+                    return redirect(next)
+                return redirect('pages:index')
             else:
                 form.add_error(None, 'Unable to log in')
         context['form'] = form
     return render(request, 'account/login.html', context)
 
+
+@login_required
 def do_logout(request):
     logout(request)
-    return render(request, 'account/login.html')
-    
+    messages.success(request, 'Signed out successfully')
+    return redirect('pages:index')
+
